d872225f0ddcdd7e663dfd6ad0cbb17e51e84faa
adam <brown-a2@users.noreply.github.com>
2022-07-26 10:34:03 +0100

    Entrypoint created but not used, site working as standard build

---
+++ entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e 
+# set -o xtrace # Uncomment this line for debugging purpose
+
+# Define the config anchor
+#MOJ_WP_ANCHOR="/* That's all, stop editing! Happy publishing. */"
+
+#wp config set MULTISITE true --raw --anchor="/* That's all, stop editing! Happy publishing. */" --placement='after' --config-file=/var/www/html
+#wp core multisite-convert
+
+#echo " Test shell is working " > filename2.txt
+
+exec "$@"
