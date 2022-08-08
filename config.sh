9b97b66e2a6b9227563490092af4b644d58f81db
adam <brown-a2@users.noreply.github.com>
2022-08-08 11:50:27 +0100

    Add Bash check around if multisite installed

--- config.sh
+++ config.sh
@@ -7,10 +7,18 @@ wp config set BLOG_ID_CURRENT_SITE 1 --raw
 wp config set SITE_ID_CURRENT_SITE 1 --raw
 wp config set SUBDOMAIN_INSTALL false --raw
 
-# Convert database to support multisite if it is a fresh db install
-wp core multisite-convert --title="Hale Platform"
+
+# Check whether WordPress Multisite is installed or not.
+# If not, then install via WP CLI
+if ! wp core is-installed --network; then
+    wp core multisite-convert --title="Hale Platform WP Multisite"
+fi
+
+# Run DB check and update
 wp core update-db --network
 
-wp plugin --network activate
+# Check plugins are activated
+wp plugin --all --network activate
 
+# Convert database to support multisite if it is a fresh db install
 exec "$@"
