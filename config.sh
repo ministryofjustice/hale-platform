694a1df3c8c68df436b1897359a4e24df7641c44
adam <brown-a2@users.noreply.github.com>
2022-08-10 21:37:43 +0100

    NGINX Multisite working + host environment variables

--- config.sh
+++ config.sh
@@ -6,7 +6,10 @@ wp config set WP_ALLOW_MULTISITE true --raw
 wp config set BLOG_ID_CURRENT_SITE 1 --raw
 wp config set SITE_ID_CURRENT_SITE 1 --raw
 wp config set SUBDOMAIN_INSTALL false --raw
-
+wp config set COOKIE_DOMAIN "\$_SERVER['HTTP_HOST']" --raw 
+wp config set DOMAIN_CURRENT_SITE "\$_SERVER['SERVER_NAME']" --raw
+wp config set WP_ENVIRONMENT_TYPE "\$_SERVER['WP_ENVIRONMENT_TYPE']" --raw 
+wp config set WP_DEBUG true --raw
 
 # Check whether WordPress Multisite is installed or not.
 # If not, then install via WP CLI
@@ -20,5 +23,4 @@ wp core update-db --network
 # Check plugins are activated
 wp plugin --all --network activate
 
-# Convert database to support multisite if it is a fresh db install
 exec "$@"
