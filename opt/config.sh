4bc72b95d97bf4bc8b38abbca63a96fe7d3e90ee
adam <brown-a2@users.noreply.github.com>
2022-08-18 11:24:35 +0100

    Modify multisite install so it works even on fresh db

--- opt/config.sh
+++ opt/config.sh
@@ -6,16 +6,20 @@ wp config set WP_ALLOW_MULTISITE true --raw
 wp config set BLOG_ID_CURRENT_SITE 1 --raw
 wp config set SITE_ID_CURRENT_SITE 1 --raw
 wp config set SUBDOMAIN_INSTALL false --raw
-wp config set COOKIE_DOMAIN "\$_SERVER['HTTP_HOST']" --raw 
 wp config set DOMAIN_CURRENT_SITE "\$_SERVER['SERVER_NAME']" --raw
+wp config set COOKIE_DOMAIN "\$_SERVER['HTTP_HOST']" --raw 
 wp config set WP_ENVIRONMENT_TYPE "\$_SERVER['WP_ENVIRONMENT_TYPE']" --raw 
 wp config set WP_DEBUG true --raw
 
-# Check whether WordPress Multisite is installed or not.
-# If not, then install via WP CLI
-if ! wp core is-installed --network; then
-    wp core multisite-convert --title="Hale Platform WP Multisite"
-fi
+#WP core install
+wp core multisite-install --title="Hale Multisite Platform" \
+    --admin_user="${WORDPRESS_ADMIN_USER}" \
+    --admin_email="${WORDPRESS_ADMIN_EMAIL}" \
+    --admin_password="${WORDPRESS_ADMIN_PASSWORD}" \
+    --url="${SERVER_NAME}" \
+    --skip-config \
+    --skip-email \
+    --quiet;
 
 # Run DB check and update
 wp core update-db --network
