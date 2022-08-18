51efa869a93bdb9a93e854adae15c94d41d3c82d
adam <brown-a2@users.noreply.github.com>
2022-08-18 11:28:14 +0100

    Clean up entrypoint shell remove unused comments

--- opt/hale-entrypoint.sh
+++ opt/hale-entrypoint.sh
@@ -2,28 +2,7 @@
 set -e 
 # set -o xtrace # Uncomment this line for debugging purpose
 
-#chmod +x /usr/local/bin/docker-entrypoint.sh
-#./usr/local/bin/docker-entrypoint.sh
-
-#wp core download
-
+# Inject shell script into docker-entrypoint so that our own conifg script can run
 sed "$ i /usr/local/bin/config.sh" /usr/local/bin/docker-entrypoint.sh > /tmp/docker-entrypoint.sh && cat /tmp/docker-entrypoint.sh > /usr/local/bin/docker-entrypoint.sh
-
-#sed -i '$ i /usr/local/bin/config.sh' /usr/local/bin/docker-entrypoint.sh
-
-
-#wp core multisite-install --path="/var/www/html" --title="Welcome to the Hale Platform Multisite build"
-#wp core multisite-convert --path="/var/www/html"
-#wp db check
-#echo "hello 2" >> pusheen.txt
-#ls | grep * > filename.txt
-#wp db check --path="/var/www/html" >> filename.txt
-
-#export WORDPRESS_ENABLE_MULTISITE="${WORDPRESS_ENABLE_MULTISITE:-yes}" # only used during the first initialization
-
-#wp config set MULTISITE true --raw
-#wp config set WP_ALLOW_MULTISITE true --raw
-
-echo "Running WP entrypoint"
 
 exec /usr/local/bin/docker-entrypoint.sh $@
