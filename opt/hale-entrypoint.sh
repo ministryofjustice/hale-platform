7b55d43916d8637b692c18b6bece8b44c44d000b
adam <brown-a2@users.noreply.github.com>
2022-12-07 21:00:15 +0000

    Fix unary syntax error

--- opt/hale-entrypoint.sh
+++ opt/hale-entrypoint.sh
@@ -3,10 +3,11 @@ set -e
 # set -o xtrace # Uncomment this line for debugging purpose
 
 VOLUME_VERSION="$(php -r 'require('"'"'/var/www/html/wp-includes/version.php'"'"'); echo $wp_version;')"
+
 echo "Volume version : "$VOLUME_VERSION
 echo "WordPress version : "$WORDPRESS_VERSION
 
-if [ $VOLUME_VERSION != $WORDPRESS_VERSION ]; then
+if [ "$VOLUME_VERSION" != "$WORDPRESS_VERSION" ]; then
     echo "Forcing WordPress code update..."
     rm -f /var/www/html/index.php
     rm -f /var/www/html/wp-includes/version.php
