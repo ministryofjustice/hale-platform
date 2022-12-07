175c8fdb44d3708aaf4dcc6bb93bc3c3ac610ba8
adam <brown-a2@users.noreply.github.com>
2022-12-07 20:32:56 +0000

    Trial script to force WP upgrade

--- opt/hale-entrypoint.sh
+++ opt/hale-entrypoint.sh
@@ -2,6 +2,16 @@
 set -e 
 # set -o xtrace # Uncomment this line for debugging purpose
 
+VOLUME_VERSION="$(php -r 'require('"'"'/var/www/html/wp-includes/version.php'"'"'); echo $wp_version;')"
+echo "Volume version : "$VOLUME_VERSION
+echo "WordPress version : "$WORDPRESS_VERSION
+
+if [ $VOLUME_VERSION != $WORDPRESS_VERSION ]; then
+    echo "Forcing WordPress code update..."
+    rm -f /var/www/html/index.php
+    rm -f /var/www/html/wp-includes/version.php
+fi
+
 # Inject shell script into docker-entrypoint so that our own conifg script can run
 sed "$ i /usr/local/bin/config.sh" /usr/local/bin/docker-entrypoint.sh > /tmp/docker-entrypoint.sh && cat /tmp/docker-entrypoint.sh > /usr/local/bin/docker-entrypoint.sh
 
