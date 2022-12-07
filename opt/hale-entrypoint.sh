2f8fe42642b271de99647633d8d44cd881d5fda6
adam <brown-a2@users.noreply.github.com>
2022-12-07 21:09:40 +0000

    Modify shell script conditional

--- opt/hale-entrypoint.sh
+++ opt/hale-entrypoint.sh
@@ -7,7 +7,7 @@ VOLUME_VERSION="$(php -r 'require('"'"'/var/www/html/wp-includes/version.php'"'"
 echo "Volume version : "$VOLUME_VERSION
 echo "WordPress version : "$WORDPRESS_VERSION
 
-if [ "$VOLUME_VERSION" != "$WORDPRESS_VERSION" ]; then
+if [[ "$VOLUME_VERSION" != "$WORDPRESS_VERSION" ]]; then
     echo "Forcing WordPress code update..."
     rm -f /var/www/html/index.php
     rm -f /var/www/html/wp-includes/version.php
