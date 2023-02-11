6fb2b4e89742916f6c8f6a26fe380d732016f4ea
adam <brown-a2@users.noreply.github.com>
2023-02-11 23:53:16 +0000

    Try empty string for cookie set

--- opt/scripts/config.sh
+++ opt/scripts/config.sh
@@ -7,7 +7,7 @@ wp config set BLOG_ID_CURRENT_SITE 1 --raw
 wp config set SITE_ID_CURRENT_SITE 1 --raw
 wp config set SUBDOMAIN_INSTALL false --raw
 wp config set DOMAIN_CURRENT_SITE "\$_SERVER['SERVER_NAME']" --raw
-wp config set COOKIE_DOMAIN "\$_SERVER['SERVER_NAME']" --raw 
+wp config set COOKIE_DOMAIN "" --raw 
 wp config set WP_ENVIRONMENT_TYPE "\$_SERVER['WP_ENVIRONMENT_TYPE']" --raw 
 #wp config set WP_DEBUG true --raw
 wp config set AUTOMATIC_UPDATER_DISABLED true --raw
