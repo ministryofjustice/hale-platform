cf074a89bb681dc49b2cd22c780b58603cb4e2a8
adam <brown-a2@users.noreply.github.com>
2023-03-22 12:06:42 +0000

    Add load.php file to autoload mu-plugins

---
+++ opt/php/load.php
@@ -0,0 +1,7 @@
+<?php
+
+// Autoloads mu-plugins
+
+require WPMU_PLUGIN_DIR .'/mu-plugins/wp-gov-uk-notify/wp-gov-uk-notify.php';
+require WPMU_PLUGIN_DIR .'/mu-plugins/wp-moj-components/wp-moj-components.php';
+require WPMU_PLUGIN_DIR .'/mu-plugins/wp-user-roles/wp-user-roles.php';
