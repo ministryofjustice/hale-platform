9c0a0fd07d8ec584da1f90ba99abcfbe5a05c1b0
adam <brown-a2@users.noreply.github.com>
2023-03-22 12:12:48 +0000

    Remove directory path duplicate causing error

--- opt/php/load.php
+++ opt/php/load.php
@@ -2,6 +2,6 @@
 
 // Autoloads mu-plugins
 
-require WPMU_PLUGIN_DIR .'/mu-plugins/wp-gov-uk-notify/wp-gov-uk-notify.php';
-require WPMU_PLUGIN_DIR .'/mu-plugins/wp-moj-components/wp-moj-components.php';
-require WPMU_PLUGIN_DIR .'/mu-plugins/wp-user-roles/wp-user-roles.php';
+require WPMU_PLUGIN_DIR .'/wp-gov-uk-notify/wp-gov-uk-notify.php';
+require WPMU_PLUGIN_DIR .'/wp-moj-components/wp-moj-components.php';
+require WPMU_PLUGIN_DIR .'/wp-user-roles/wp-user-roles.php';
