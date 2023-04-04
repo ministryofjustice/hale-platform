26ca39a8e7585b958674b52904de48eb46dc0328
adam <brown-a2@users.noreply.github.com>
2023-04-04 14:22:34 +0100

    Disable Sentry

--- opt/php/load.php
+++ opt/php/load.php
@@ -9,5 +9,5 @@ require WPMU_PLUGIN_DIR .'/wp-s3-uploads/s3-uploads.php';
 
 
 // Load Sentry
-\Sentry\init(['dsn' => 'https://f1d6c41335d94ca49536a80ed1d6ae1c@o345774.ingest.sentry.io/4504955899478016' ]);
-\Sentry\captureLastError();
+#\Sentry\init(['dsn' => 'https://f1d6c41335d94ca49536a80ed1d6ae1c@o345774.ingest.sentry.io/4504955899478016' ]);
+#\Sentry\captureLastError();
