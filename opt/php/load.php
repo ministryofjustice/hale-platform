3bf60b92bbab746d9c711af6dfcab62a7993c4d1
adam <brown-a2@users.noreply.github.com>
2023-04-04 22:19:06 +0100

    Remove require

--- opt/php/load.php
+++ opt/php/load.php
@@ -9,12 +9,11 @@ require WPMU_PLUGIN_DIR .'/wp-moj-components/wp-moj-components.php';
 require WPMU_PLUGIN_DIR .'/wp-user-roles/wp-user-roles.php';
 require WPMU_PLUGIN_DIR .'/wp-s3-uploads/s3-uploads.php';
 
-require $root_dir . '/vendor/autoload.php';
-
 /**
  * Initialise Sentry
  */
 if (function_exists('sentry\init')) {
     Sentry\init(['dsn' => 'https://f1d6c41335d94ca49536a80ed1d6ae1c@o345774.ingest.sentry.io/4504955899478016' ]);
     Sentry\captureLastError();
+    echo 'hello world';
 }
