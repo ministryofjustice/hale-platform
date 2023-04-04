55c51ae1a45f22dba910378fdcc5b1d31c5125f2
adam <brown-a2@users.noreply.github.com>
2023-04-04 22:56:16 +0100

    Graceful notice

--- opt/php/load.php
+++ opt/php/load.php
@@ -1,7 +1,5 @@
 <?php
 
-$root_dir = dirname(__DIR__);
-
 // Autoloads mu-plugins
 
 require WPMU_PLUGIN_DIR .'/wp-gov-uk-notify/wp-gov-uk-notify.php';
@@ -15,5 +13,6 @@ require WPMU_PLUGIN_DIR .'/wp-s3-uploads/s3-uploads.php';
 if (function_exists('sentry\init')) {
     Sentry\init(['dsn' => 'https://f1d6c41335d94ca49536a80ed1d6ae1c@o345774.ingest.sentry.io/4504955899478016' ]);
     Sentry\captureLastError();
-    echo 'hello world';
+} else {
+    trigger_error('Sentry not loaded.', $error_level = E_USER_NOTICE);
 }
