f3d7b3846aef9cb848b38be98f28aa1898a893e7
adam <brown-a2@users.noreply.github.com>
2023-04-04 21:50:57 +0100

    Add autoload support

--- opt/php/load.php
+++ opt/php/load.php
@@ -1,5 +1,7 @@
 <?php
 
+$root_dir = dirname(__DIR__);
+
 // Autoloads mu-plugins
 
 require WPMU_PLUGIN_DIR .'/wp-gov-uk-notify/wp-gov-uk-notify.php';
@@ -7,7 +9,8 @@ require WPMU_PLUGIN_DIR .'/wp-moj-components/wp-moj-components.php';
 require WPMU_PLUGIN_DIR .'/wp-user-roles/wp-user-roles.php';
 require WPMU_PLUGIN_DIR .'/wp-s3-uploads/s3-uploads.php';
 
+require $root_dir . '/wp-content/vendor/autoload.php';
 
 // Load Sentry
-#\Sentry\init(['dsn' => 'https://f1d6c41335d94ca49536a80ed1d6ae1c@o345774.ingest.sentry.io/4504955899478016' ]);
-#\Sentry\captureLastError();
+\Sentry\init(['dsn' => 'https://f1d6c41335d94ca49536a80ed1d6ae1c@o345774.ingest.sentry.io/4504955899478016' ]);
+\Sentry\captureLastError();
