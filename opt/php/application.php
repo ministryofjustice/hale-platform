e388198b25c2b91f604da408a04785b95121af7a
adam <brown-a2@users.noreply.github.com>
2023-04-05 12:45:20 +0100

    Replace replica count to 1 for testing

--- opt/php/application.php
+++ opt/php/application.php
@@ -3,7 +3,7 @@
 /** @var string Directory containing all of the site's files */
 $root_dir = dirname(__DIR__);
 
-require $root_dir . '/wp-content/vendor/autoload.php';
+require $root_dir . 'html/wp-content/vendor/autoload.php';
 
 /**
  * Initialise Sentry
@@ -11,7 +11,4 @@ require $root_dir . '/wp-content/vendor/autoload.php';
 if (function_exists('sentry\init')) {
     Sentry\init(['dsn' => 'https://f1d6c41335d94ca49536a80ed1d6ae1c@o345774.ingest.sentry.io/4504955899478016' ]);
     Sentry\captureLastError();
-    trigger_error('Sentry loaded.', $error_level = E_USER_NOTICE);
-} else {
-    trigger_error('Sentry not loaded.', $error_level = E_USER_NOTICE);
-}
+} 
