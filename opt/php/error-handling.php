d30f250de1e21f315dfba673a407cd6414b86a8e
adam <brown-a2@users.noreply.github.com>
2023-05-02 12:42:33 +0100

    Simplify error handling

--- opt/php/error-handling.php
+++ opt/php/error-handling.php
@@ -2,45 +2,9 @@
 
 $envType = getenv('ENV_TYPE');
 
-if ($envType === 'demo') {
+if ($envType === 'dev') {
 
-    // Turn on error reporting
     error_reporting(E_ALL);
-
-    // Exclude deprecated warnings
-    error_reporting(error_reporting() & ~E_DEPRECATED);
-
-    $errorConstants = [
-        'WP_DEBUG' => 'true',
-        'WP_DEBUG_LOG' => 'true',
-        'WP_DEBUG_DISPLAY' => 'true'
-    ];
-
 } else {
-
-    @ini_set( 'display_errors', 0 );
-    error_reporting(error_reporting(0) & ~E_DEPRECATED);
-
-    $errorConstants = [
-        'WP_DEBUG' => 'true',
-        'WP_DEBUG_LOG' => 'true',
-        'WP_DEBUG_DISPLAY' => 'false'
-    ];
+    error_reporting(0);
 }
-
-foreach ($errorConstants as $errorConstant => $value) {
-
-    // Set the WP-CLI command to run
-    $command = "wp config set $errorConstant $value --raw";
-
-    // Execute the command using exec()
-    exec($command, $output, $return_var);
-
-    // Check the return status
-    if ($return_var !== 0) {
-        echo 'WP-CLI command failed';
-        exit;
-    }
-
-}
-
