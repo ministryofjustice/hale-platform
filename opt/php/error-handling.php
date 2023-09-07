7ef782f60e6dd38c25330b685ee73875984cc352
adam <brown-a2@users.noreply.github.com>
2023-09-07 21:37:51 +0100

    Remove WP Error handling

--- opt/php/error-handling.php
+++ opt/php/error-handling.php
@@ -3,15 +3,6 @@
 $envType = getenv('ENV_TYPE');
 
 if ($envType === 'dev' || $envType === 'local') {
-    // Enable debugging
-    define('WP_DEBUG', true);
-
-    // Log errors to a file
-    define('WP_DEBUG_LOG', true);
-
-    // Display errors on the webpage (optional)
-    define('WP_DEBUG_DISPLAY', false);
-
     ini_set('display_errors', 0);
     ini_set('log_errors', 1);
     ini_set('error_log', 'syslog');
