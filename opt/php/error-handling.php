62ceda86c67d2ad965fb40f650b9a06d8fd7d756
adam <brown-a2@users.noreply.github.com>
2023-09-07 21:27:54 +0100

    Test new error handling method

--- opt/php/error-handling.php
+++ opt/php/error-handling.php
@@ -2,9 +2,20 @@
 
 $envType = getenv('ENV_TYPE');
 
-if ($envType === 'dev') {
+if ($envType === 'dev' || $envType === 'local') {
+    // Enable debugging
+    define('WP_DEBUG', true);
 
-    error_reporting(E_ALL);
+    // Log errors to a file
+    define('WP_DEBUG_LOG', true);
+
+    // Display errors on the webpage (optional)
+    define('WP_DEBUG_DISPLAY', false);
+
+    ini_set('display_errors', 0);
+    ini_set('log_errors', 1);
+    ini_set('error_log', 'syslog');
+    error_log("This is an error message for the development environment.");
 } else {
     error_reporting(0);
 }
