d82c18cb235fe680b01363e310e51296d1ef9533
EarthlingDavey <15802017+EarthlingDavey@users.noreply.github.com>
2025-12-09 17:31:53 +0000

    Revert "Modify `docker-entrypoint.sh` `targetTarArgs` so that tar doesn't try to set permissions and fail."
    This reverts commit 40500f5d29216161d124a6c874d047af9094e4c1.

--- opt/scripts/hale-entrypoint.sh
+++ opt/scripts/hale-entrypoint.sh
@@ -4,12 +4,6 @@ set -e
 
 # Inject shell script into docker-entrypoint so that our own config script can run
 sed "$ i /usr/local/bin/config.sh" /usr/local/bin/docker-entrypoint.sh > /tmp/docker-entrypoint.sh
-
-# Modify `targetTarArgs` to add `--no-same-owner --no-same-permissions` flags
-# This means that tar will not attempt to modify permissions from 0755 to 0777, 
-# which fails on Cloud Platform, because /var/www/html is owned by root.
-sed -i 's/--no-overwrite-dir/--no-overwrite-dir --no-same-owner --no-same-permissions/g' /tmp/docker-entrypoint.sh
-
 cat /tmp/docker-entrypoint.sh > /usr/local/bin/docker-entrypoint.sh
 
 # Execute the modified docker-entrypoint.sh
