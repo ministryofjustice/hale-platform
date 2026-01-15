e7f458ba762e71a72bb1e1f590aa74df2b05b88c
Daniel Glen <daniel.glen@digital.justice.gov.uk>
2026-01-15 14:31:52 +0000

    Added new secret for managing S3 Uploads Object ACL

--- opt/scripts/config.sh
+++ opt/scripts/config.sh
@@ -19,6 +19,7 @@ wp config set S3_UPLOADS_REGION "\$_SERVER['S3_UPLOADS_REGION']" --raw
 wp config set S3_UPLOADS_BUCKET_URL "\$_SERVER['S3_UPLOADS_BUCKET_URL']" --raw
 wp config set S3_UPLOADS_USE_INSTANCE_PROFILE "\$_SERVER['S3_UPLOADS_USE_INSTANCE_PROFILE']" --raw
 wp config set S3_UPLOADS_BUCKET_URL "\$_SERVER['S3_UPLOADS_BUCKET_URL']" --raw
+wp config set S3_UPLOADS_OBJECT_ACL "\$_SERVER['S3_UPLOADS_OBJECT_ACL']" --raw
 wp config set QM_ENABLE_CAPS_PANEL true --raw
 wp config set WP_CACHE true --raw
 wp config set ACF_PRO_LICENSE "\$_SERVER['ACF_PRO_LICENSE']" --raw
