67b52d3ed67ab06a2de31ed081dc93e79641c7d1
adam <brown-a2@users.noreply.github.com>
2026-03-16 11:33:05 +0000

    Use default script for now

--- opt/scripts/hale-entrypoint.sh
+++ opt/scripts/hale-entrypoint.sh
@@ -2,13 +2,9 @@
 set -e
 #set -o xtrace # Uncomment this line for debugging purposes
 
-echo "[hale-entrypoint] Starting WordPress multisite setup..."
-
 # Inject shell script into docker-entrypoint so that our own config script can run
 sed "$ i /usr/local/bin/config.sh" /usr/local/bin/docker-entrypoint.sh > /tmp/docker-entrypoint.sh
 cat /tmp/docker-entrypoint.sh > /usr/local/bin/docker-entrypoint.sh
 
-echo "[hale-entrypoint] Starting WordPress initialization and PHP-FPM..."
-# Execute the modified docker-entrypoint.sh with php-fpm
-# This will run in foreground and keep the container alive
-exec /usr/local/bin/docker-entrypoint.sh "php-fpm"
+# Execute the modified docker-entrypoint.sh
+/usr/local/bin/docker-entrypoint.sh "php-fpm"
