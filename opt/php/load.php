54d0ebc59eacf3c9d57100fe6fa0846c119b5d13
EarthlingDavey <15802017+EarthlingDavey@users.noreply.github.com>
2026-04-29 16:05:40 +0100

    Add compatibility for mu-plgins and dev symlinks.

--- opt/php/load.php
+++ opt/php/load.php
@@ -11,3 +11,35 @@ require WPMU_PLUGIN_DIR .'/hale-components/hale-components.php';
 if (getenv('WP_ENVIRONMENT_TYPE') != 'local') {
     require WPMU_PLUGIN_DIR .'/wp-s3-uploads/s3-uploads.php';
 }
+
+/**
+ * Replace symlink mount path (/mnt/dev/) with target path (/var/www/html/wp-content/).
+ *
+ * This fixes an issue where the value for `$plugin` is a path that WordPress doesn't recognise,
+ * e.g. /mnt/dev/mu-plugins/hale-components/moj-components/component/Users/UserSwitch.php
+ *
+ * @param string $url    The complete URL to the plugins directory including scheme and path.
+ * @param string $path   Path relative to the URL to the plugins directory. Blank string
+ *                       if no path is specified.
+ * @param string $plugin The plugin file path to be relative to. Blank string if no plugin
+ *                       is specified.
+ */
+function wb_reformat_mu_plugin_urls($url, $path, $plugin)
+{
+    // If $plugin doesn't start with the mount path, then do nothing.
+    if (!str_starts_with($plugin, '/mnt/dev/mu-plugins/')) {
+        return $url;
+    }
+
+    // Replace symlink mount path (/mnt/dev) with 
+    // target path WP_CONTENT_DIR (e.g. /var/www/html/wp-content).
+    $plugin_reformatted = str_replace('/mnt/dev/mu-plugins', WPMU_PLUGIN_DIR, $plugin);
+
+    // Recall plugins_url with the reformatted path.
+    // No infinite loop, because we replaced `/mnt/dev`
+    return plugins_url($path, $plugin_reformatted);
+}
+
+if (getenv('WP_ENVIRONMENT_TYPE') === 'local') {
+    add_filter('plugins_url', 'wb_reformat_mu_plugin_urls', 10, 3);
+}
